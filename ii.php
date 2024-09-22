9de24ecaea184965fc640f8d93cdf6cc2d6ab269
Serednii Mykola <seredniimykola@gmail.com>
2024-09-22 20:37:23 +0200

    Updates

---
+++ ii.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>websites</title>
+
+    <link rel="stylesheet" href="./css1/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <a class="link-github" href="https://github.com/serednii">github</a>
+        <ul class="menu">
+            <li class="link">
+                <a target="blank" href="https://serednii.github.io/wawe/">
+                    <img loading="lazy" src="images/wawe.jpg" alt="wawe">
+                    <h2 class="name-site">Wawe</h2>
+                </a>
+            </li>
+            <li class="link">
+                <a target="blank" href="https://serednii.github.io/Marketing/">
+                    <img loading="lazy" src="images/marketing.jpg" alt="marketing">
+                    <h2 class="name-site">Marketing</h2>
+                </a>
+            </li>
+            <li class="link">
+                <a target="blank" href="http://glee.smm.zzz.com.ua">
+                    <img loading="lazy" src="images/glee.jpg" alt="glee">
+                    <h2 class="name-site">Glee</h2>
+                </a>
+            </li>
+            <li class="link">
+                <a target="blank" href="http://marcho.smm.zzz.com.ua">
+                    <img loading="lazy" src="images/marcho.jpg" alt="marcho">
+                    <h2 class="name-site">Marcho</h2>
+                </a>
+            </li>
+            <li class="link">
+                <a target="blank" href="http://learn.nastya03.zzz.com.ua">
+                    <img loading="lazy" src="images/online_learn.jpg" alt="online_learn">
+                    <h2 class="name-site">Online Learn</h2>
+                </a>
+            </li>
+
+            <li class="link">
+                <a target="blank" href="http://modernart.nastya03.zzz.com.ua">
+                    <img loading="lazy" src="images/mordern_art.jpg" alt="mordern_art">
+                    <h2 class="name-site">Modern Art</h2>
+                </a>
+            </li>
+            <li class="link">
+                <a target="blank" href="http://hytte.nastya03.zzz.com.ua">
+                    <img loading="lazy" src="images/hytte.jpg" alt="hytte">
+                    <h2 class="name-site">Hytte</h2>
+                </a>
+
+            </li>
+            <li class="link">
+                <a target="blank" href="https://serednii.github.io/weekend/">
+                    <img loading="lazy" src="images/weekend.jpg" alt="weekend">
+                    <h2 class="name-site">Weekend</h2>
+                </a>
+
+            </li>
+            <li class="link">
+                <a target="blank"href="http://sneakers.smm.zzz.com.ua/">
+                    <img loading="lazy"src="images/sneakers.jpg" alt="sneakers">
+                    <h2 class="name-site">Sneakers</h2>
+                </a>
+            </li>
+            <li class="link">
+                <a target="blank" href="http://shop.smm.zzz.com.ua">
+                    <img loading="lazy" src="images/shop.jpg" alt="shop">
+                    <h2 class="name-site">Shop</h2>
+                </a>
+            </li>
+            <li class="link">
+                <a target="blank" href="http://miroproject.smm.zzz.com.ua/old.html">
+                    <img loading="lazy" src="images/arduino.jpg" alt="Arduino">
+                    <h2 class="name-site">Arduino</h2>
+                </a>
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html
